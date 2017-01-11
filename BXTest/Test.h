1c4d34a9bff16e57ed06a4c2d77eb9e89ca1a9d4
baxiaxx <285558940@qq.com>
2017-01-11 10:05:24 +0800

    add pod files

---
+++ BXTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by Moxian_X on 16/12/13.
+//  Copyright © 2016年 Moxian_X. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
++ (void)abc;
+@end
---
+++ BXTest/Test.m
@@ -0,0 +1,15 @@
+//
+//  Test.m
+//  Test
+//
+//  Created by Moxian_X on 16/12/13.
+//  Copyright © 2016年 Moxian_X. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
++ (void)abc {
+    NSLog(@"%@",@"test123");
+}
+@end
